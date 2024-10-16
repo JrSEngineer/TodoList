4084d71fea433df295f6f391137d82ede07c6702
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-10-15 21:29:53 -0300

    :building_construction: Adding Cors policy for development.

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("development-policy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
