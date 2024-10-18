701582019fc51e81dc14e961b4c98e186f716eb0
JrSEngineer <jr.s.engineer@gmail.com>
2024-10-17 21:03:25 -0300

    :bricks: Adding InProgress property to Todo.

---
+++ TodoList.Infra/Migrations/20241018000220_ModifyingTodoTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyingTodoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "InProgress",
+                table: "ToDo",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InProgress",
+                table: "ToDo");
+        }
+    }
+}
